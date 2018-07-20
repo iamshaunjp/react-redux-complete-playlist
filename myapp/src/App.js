111cab33f8a538962af8c75cd86a62e95f415f08
Shaun <shaunjp@shaun-pelling.com>
2018-07-20 16:45:08 +0100

    lesson-12

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <p>Welcome :)</p>
+        <Ninjas />
       </div>
     );
   }
