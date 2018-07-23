1ddc3418eff00c9074b4ec689a75964437096c11
Shaun <shaunjp@shaun-pelling.com>
2018-07-23 15:15:49 +0100

    lesson-25

---
+++ poketimes/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar'
+import { Route, BrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
