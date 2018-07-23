e9f600f859d8df664116615364d5b1f43236c5f7
Shaun <shaunjp@shaun-pelling.com>
2018-07-23 10:04:58 +0100

    lesson-17

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
 
 class App extends Component {
   state = {
@@ -14,6 +15,7 @@ class App extends Component {
       <div className="App">
         <h1>My first React app</h1>
         <Ninjas ninjas={this.state.ninjas}/>
+        <AddNinja />
       </div>
     );
   }
