8dbc1393ffe2785cc54e77d6d9f05bbeea6405f6
Shaun <shaunjp@shaun-pelling.com>
2018-07-23 21:51:22 +0100

    lesson-43

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { deletePost } from '../actions/postActions'
 
 class Post extends Component {
   handleClick = () => {
@@ -39,7 +40,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    deletePost: (id) => dispatch({type: 'DELETE_POST', id: id})
+    deletePost: (id) => dispatch(deletePost(id))
   }
 }
 
