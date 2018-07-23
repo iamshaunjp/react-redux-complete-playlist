537182361dc1392d553014ffd04aeaf751e07c92
Shaun <shaunjp@shaun-pelling.com>
2018-07-23 21:44:08 +0100

    lesson-42

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Post extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/');
+  }
   render() {
 
     const post = this.props.post ? (
@@ -9,7 +13,7 @@ class Post extends Component {
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
         <div className="center">
-          <button className="btn grey" onClick={() => {this.props.deletePost(this.props.post.id)}}>
+          <button className="btn grey" onClick={this.handleClick}>
             Delete Post
           </button>
         </div>
@@ -27,7 +31,7 @@ class Post extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  let id = parseInt(ownProps.match.params.post_id);
+  let id = ownProps.match.params.post_id;
   return {
     post: state.posts.find(post => post.id === id)
   }
