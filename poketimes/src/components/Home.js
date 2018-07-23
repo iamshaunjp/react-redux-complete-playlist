5b1f8884881edf05a537ae1e88090d7a47921d79
Shaun <shaunjp@shaun-pelling.com>
2018-07-23 17:20:45 +0100

    lesson-31

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
   state = {
@@ -21,7 +22,9 @@ class Home extends Component {
         return (
           <div className="post card" key={post.id}>
             <div className="card-content">
-              <span className="card-title">{post.title}</span>
+              <Link to={'/' + post.id}>
+                <span className="card-title red-text">{post.title}</span>
+              </Link>
               <p>{post.body}</p>
             </div>
           </div>
--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Post extends Component {
   state = {
-    id: null
+    post: null
   }
   componentDidMount(){
     let id = this.props.match.params.post_id;
-    this.setState({
-      id
-    })
+    axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+      .then(res => {
+        this.setState({
+          post: res.data
+        });
+        //console.log(res.data);
+      });
   }
   render() {
+
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading post...</div>
+    );
+
     return (
       <div className="container">
-        <h4>{this.state.id}</h4>
+        {post}
       </div>
     )
   }
