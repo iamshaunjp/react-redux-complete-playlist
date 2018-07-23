4540cf4afe7a5d558aac4b37414405f8ed2f9a66
Shaun <shaunjp@shaun-pelling.com>
2018-07-23 17:35:05 +0100

    lesson-33

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 class Home extends Component {
   state = {
@@ -21,6 +22,7 @@ class Home extends Component {
       posts.map(post => {
         return (
           <div className="post card" key={post.id}>
+            <img src={Pokeball} alt="A Pokeball" />
             <div className="card-content">
               <Link to={'/' + post.id}>
                 <span className="card-title red-text">{post.title}</span>
@@ -36,7 +38,7 @@ class Home extends Component {
 
     return (
       <div>
-        <div className="container">
+        <div className="container home">
           <h4 className="center">Home</h4>
           {postList}
         </div>
