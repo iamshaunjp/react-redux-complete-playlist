cd3107e759735acc8c03cf18cb5962595af00193
Shaun <shaunjp@shaun-pelling.com>
2018-07-23 17:25:06 +0100

    lesson-32

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -12,10 +12,12 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/:post_id' component={Post} />
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/:post_id' component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
