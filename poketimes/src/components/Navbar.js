6f672035b6055816b989faa73ce4cc4d61d2766b
Shaun <shaunjp@shaun-pelling.com>
2018-07-23 15:24:11 +0100

    lesson-26

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Poke' Times</a>
+        <Link className="brand-logo" to="/">Poke' Times</Link>
         <ul className="right">
-          <li><a href="/">Home</a></li>
-          <li><a href='/about'>About</a></li>
-          <li><a href='/contact'>Contact</a></li>
+          <li><NavLink exact to="/">Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <li><NavLink to='/contact'>Contact</NavLink></li>
         </ul>
       </div>
     </nav> 
