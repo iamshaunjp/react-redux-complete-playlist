a58c23e68d382e16859f552b7abdedb5351e6da1
louatiSaadeddine <saad.louati@galactechstudio.com>
2019-11-16 12:06:47 +0100

    addNinja Form Restyling

--- myapp/src/AddNinja.js
+++ myapp/src/AddNinja.js
@@ -19,11 +19,12 @@ class AddNinja extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Name:</label>
+          <br/>
+          <label htmlFor="name"> Name: </label>
           <input type="text" id="name" onChange={this.handleChange} />
-          <label htmlFor="age">Age:</label>
+          <label htmlFor="age"> Age: </label>
           <input type="text" id="age" onChange={this.handleChange} />
-          <label htmlFor="belt">Belt:</label>
+          <label htmlFor="belt"> Belt: </label>
           <input type="text"id="belt" onChange={this.handleChange} />
           <button>Submit</button>
         </form>
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,9 +5,7 @@ import AddNinja from './AddNinja'
 class App extends Component {
   state = {
     ninjas: [
-      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+      { name: 'Saadeddine LOUATI', age: 24, belt: 'black', id: 1 }
     ]
   }
   addNinja = (ninja) => {
